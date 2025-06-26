ec381352fc55df979039c1851d2b30b08f2e1f95
manjosh1990 <manjosh.1990@yahoo.com>
2025-06-26 22:29:00 +0530

    upload icon image

--- invoice-generator-gui/src/assets/assets.js
+++ invoice-generator-gui/src/assets/assets.js
@@ -3,10 +3,13 @@ import landing1 from './landing1.png';
 import landing2 from './landing2.png';
 import landing3 from './landing3.png';
 import landing4 from './landing4.png';
+import upload_area from './upload.png';
 export const assets = {
     logo,
     landing1,
     landing2,
     landing3,
     landing4,
+    upload_area,
+
 }
