4fe337511a81a60f4bbfca58baa0ec385db347c6
manjosh1990 <manjosh.1990@yahoo.com>
2025-06-26 22:28:19 +0530

    wrapped with app context

--- invoice-generator-gui/src/main.jsx
+++ invoice-generator-gui/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import {AppContextProvider} from "./context/AppContext.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <AppContextProvider>
+        <App/>
+    </AppContextProvider>,
 )
